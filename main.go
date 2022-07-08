e5fca48b110512e6d202af28e095d123bb3b4c98
Karelia <sunagimogushi0406@gmail.com>
2022-07-08 16:55:52 +0900

    ポートを80番に

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
+
 import "github.com/gin-gonic/gin"
+
 func main() {
-  r := gin.Default()
-  r.GET("/health", func(c *gin.Context) {
-    c.JSON(200, gin.H{
-      "status": "ok",
-    })
-  })
-  r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r := gin.Default()
+	r.GET("/health", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"status": "ok",
+		})
+	})
+	r.Run(":80") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
