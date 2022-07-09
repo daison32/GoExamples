994ccc4d43563278f18011707660923d8488a371
Karelia <sunagimogushi0406@gmail.com>
2022-07-09 18:24:52 +0900

    修正戻し

--- main.go
+++ main.go
@@ -1,29 +1,15 @@
 package main
 
 import (
-	"/handler.go"
-
 	"github.com/gin-gonic/gin"
 )
-// func main() {
-// 	r := gin.Default()
-// 	r.GET("/health", func(c *gin.Context) {
-// 		c.JSON(200, gin.H{
-// 			"status": "ok",
-// 		})
-// 	})
-// 	// r.Run(":80") ｚ
-// }
 
 func main() {
 	r := gin.Default()
-
 	r.GET("/health", func(c *gin.Context) {
-        // 値を取得する
-		tasks := handler.GetAll()
-        // JSONで返す
-		c.JSON(200, tasks)
+		c.JSON(200, gin.H{
+			"status": "ok",
+		})
 	})
-
 	r.Run()
 }
