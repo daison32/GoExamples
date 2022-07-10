4ae5f2abf96959032a9deb5d65f79c384717e847
Karelia <sunagimogushi0406@gmail.com>
2022-07-10 22:11:25 +0900

    cors

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"net/http"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -8,6 +11,25 @@ import (
 
 func main() {
 	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		// アクセス許可するオリジン
+		AllowOrigins: []string{
+			"*",
+		},
+		// アクセス許可するHTTPメソッド
+		AllowMethods: []string{
+			http.MethodGet,
+			http.MethodPut,
+			http.MethodPost,
+			http.MethodDelete,
+			http.MethodPatch,
+		},
+		// 許可するHTTPリクエストヘッダ
+		AllowHeaders: []string{
+			"*",
+		},
+	}))
+
 	r.GET("/tasks", tasksGet)
 
 	r.GET("/", func(c *gin.Context) {
